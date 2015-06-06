e69c557ed86d373df101934a53909650451d06e9
Manuel Iglesias <iglesiasmanu@gmail.com>
2015-06-06 00:19:28 -0300

    debug
    estoy teniendo problemas para utilizar los Char, y me dan "a" not in
    scope, "z" not in scope etc.

--- Cesar.hs
+++ Cesar.hs
@@ -1,26 +1,35 @@
+import Data.Char
+
+
 -- lista de minusculas de la "a" a la "z" para fasilitar otras tareas
-min = "abcdefghijklmnopqrstuvwxyz"
+mIn = [a..z]
 
 -- "a" pertenece a una lista
+pertenece :: Char -> [Char] -> Bool
 pertenece a [] = False
-pertenece a (b,bs) | a == b || pertenece a bs
+pertenece a (b:bs) | a == b || pertenece a bs = True
 
-esMin a = pertenece a min
+esMin :: Char -> Bool				   
+esMin a = pertenece a mIn
 
 
 -- compara un elemento con una lista y le da un valor segun el primer momento en que aparece
+valor :: Char -> [Char] -> Integer
 valor a [] = 99999
 -- el caso vasio no va a ser utilizado mas que para poder dar un caso bace a la recurrencia
-valor a (b,bs) | a == b = 0
+valor a (b:bs) | a == b = 0
                | otherwise = 1+(valor a bs)
 
-letANat a | esMin a = valor a min
-          | otherwise = a
+-- letANat no puede ser exaustiva por la definicion de tipos.
+letANat :: Char -> Integer
+letANat a | esMin a = valor a mIn
+
+
 -- generalizacion de natALet para cualquier lista
-toMin 0 (b,bs) = b
-toMin n (b,bs) | 25 >= n = toMin (n-1) bs
+toMin 0 (b:bs) = b
+toMin n (b:bs) | 25 >= n = toMin (n-1) bs
  
-natALet n | 25 >= n = toMin n min
+natALet n | 25 >= n = toMin n mIn
           | otherwise = natALet (n-26) 
 
 desplazar 0 a = a
