c29c0e8094b901c80aa9f18c8d6b9d7e2b75a486
Manuel Iglesias <iglesiasmanu@gmail.com>
2015-06-06 18:54:20 -0300

    Codificar

--- Cesar.hs
+++ Cesar.hs
@@ -1,8 +1,10 @@
-import Data.Char
+import Data.Char 
+import Data.String
 
 
 -- lista de minusculas de la "a" a la "z" para fasilitar otras tareas
 mIn = ['a'..'z']
+tdA = "taller de algebra"
 
 -- "a" pertenece a una lista
 pertenece :: Char -> [Char] -> Bool
@@ -18,7 +20,7 @@ letANat a | esMin a = toInteger $ (ord  a)-97  -- Valor de ASCII de la letra
 
 -- generalizacion de natALet para cualquier lista
 natALet :: Integer -> Char
-natALet  n | n <= 25 && n >= 0 = chr $ fromInteger n+97
+natALet  n = chr $ fromInteger (mod n 26)+97
 
 desplazar :: Integer -> Char -> Char
 desplazar n l | esMin l = natALet $ mod ((letANat l) + n) 26
@@ -27,3 +29,17 @@ cantMinusc :: String -> Integer
 cantMinusc [] = 0
 cantMinusc (b:bs) | esMin b = 1+(cantMinusc bs)
                   | otherwise = cantMinusc bs
+--
+
+contar :: Char -> String -> Integer
+contar a [] = 0
+contar a (b:bs) | a==b = 1+contar a bs
+                | otherwise = contar a bs
+--
+listar [] = []
+listar (b:bs)= b:(listar bs)
+
+codificar :: Integer -> String -> String
+codificar n [] = []
+codificar n (b:bs) | esMin b = (desplazar n b): codificar n bs
+                   | otherwise = b : codificar n bs
